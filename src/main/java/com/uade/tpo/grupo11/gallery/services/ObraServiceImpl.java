147845643094e1d26f0fb45b37cdb3d8b5ed015f
Pili-v <pilarvillaverde05@gmail.com>
2026-08-23 19:30:32 -0300

    -CRUD obra (#2)

--- src/main/java/com/uade/tpo/grupo11/gallery/services/ObraServiceImpl.java
+++ src/main/java/com/uade/tpo/grupo11/gallery/services/ObraServiceImpl.java
@@ -1,4 +1,13 @@
 package com.uade.tpo.grupo11.gallery.services;
 
-public class ObraServiceImpl {
+import com.uade.tpo.grupo11.gallery.entities.Obra;
+import com.uade.tpo.grupo11.gallery.repositories.ObraRepository;
+
+public class ObraServiceImpl implements ObraService {
+    ObraRepository repoObra;
+
+    @Override
+    public Obra getObraById(Long obra_id) {
+        return null;
+    }
 }
