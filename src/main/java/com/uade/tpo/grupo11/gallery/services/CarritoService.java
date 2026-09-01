7e4ac4a1fb028ba0aa78642cea873ffac2088011
Thiago Brizuela <thiagobrizuela06@gmail.com>
2026-09-01 08:56:40 -0300

    Clase Carrito completa

---
+++ src/main/java/com/uade/tpo/grupo11/gallery/services/CarritoService.java
@@ -0,0 +1,19 @@
+package com.uade.tpo.grupo11.gallery.services;
+
+import java.util.List;
+
+import com.uade.tpo.grupo11.gallery.controllers.Carrito.CarritoRequest;
+import com.uade.tpo.grupo11.gallery.entities.Carrito;
+
+public interface CarritoService {
+
+    List<Carrito> getCarritos();
+
+    Carrito getCarritoById(Long carritoId);
+
+    Carrito createCarrito(CarritoRequest request);
+
+    Carrito updateCarrito(Long carritoId, CarritoRequest request);
+
+    void deleteCarrito(Long carritoId);
+}
