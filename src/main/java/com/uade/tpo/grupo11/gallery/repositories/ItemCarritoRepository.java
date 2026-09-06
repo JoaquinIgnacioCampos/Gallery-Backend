ede7624730cc15cd0f139585b9ae1cf3d1c38e47
Ph1lo1 <joaquin.ig.campos@gmail.com>
2026-09-06 19:41:54 -0300

    fix: Carrito has empty endpoint and uses DTOs for responses

--- src/main/java/com/uade/tpo/grupo11/gallery/repositories/ItemCarritoRepository.java
+++ src/main/java/com/uade/tpo/grupo11/gallery/repositories/ItemCarritoRepository.java
@@ -12,4 +12,5 @@ public interface ItemCarritoRepository extends JpaRepository<ItemCarrito, Long> 
     // Los items de un carrito: es lo que lee el checkout.
     List<ItemCarrito> findByCarritoId(Long carritoId);
 
+    void deleteByCarritoId(Long carritoId);
 }
