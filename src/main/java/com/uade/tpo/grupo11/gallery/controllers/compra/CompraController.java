b08910a1f82f3ac1b5dda22b0f258b3981279398
Ph1lo1 <joaquin.ig.campos@gmail.com>
2026-09-07 07:41:53 -0300

    Revert "Delete src/main/java/com/uade/tpo/grupo11/gallery/controllers/compra/CompraController.java"
    This reverts commit 5f43e54ff108fc26e88648263830135b6c51a220.

---
+++ src/main/java/com/uade/tpo/grupo11/gallery/controllers/compra/CompraController.java
@@ -0,0 +1,67 @@
+package com.uade.tpo.grupo11.gallery.controllers.compra;
+
+import com.uade.tpo.grupo11.gallery.services.compra.CompraService;
+
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/compras")
+public class CompraController {
+
+    @Autowired
+    private CompraService compraService;
+
+
+    // GET - Obtener todas las compras
+    @GetMapping
+    public List<CompraResponse> getCompras() {
+
+        return compraService.getCompras().stream()
+                .map(CompraResponse::fromEntity)
+                .toList();
+    }
+
+
+    // GET - Obtener una compra por ID
+    @GetMapping("/{compraId}")
+    public CompraResponse getCompraById(
+            @PathVariable Long compraId) {
+
+        return CompraResponse.fromEntity(compraService.getCompraById(compraId));
+    }
+
+
+    // POST - Crear compra
+    @PostMapping
+    public CompraResponse createCompra(
+            @Valid @RequestBody CompraRequest request) {
+
+        return CompraResponse.fromEntity(compraService.createCompra(request));
+    }
+
+
+    // PUT - Modificar compra
+    @PutMapping("/{compraId}")
+    public CompraResponse updateCompra(
+            @PathVariable Long compraId,
+            @Valid @RequestBody CompraRequest request) {
+
+        return CompraResponse.fromEntity(compraService.updateCompra(
+                compraId,
+                request
+        ));
+    }
+
+
+    // DELETE - Eliminar compra
+    @DeleteMapping("/{compraId}")
+    public void deleteCompra(
+            @PathVariable Long compraId) {
+
+        compraService.deleteCompra(compraId);
+    }
+}
